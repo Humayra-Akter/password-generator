11facf45061ffe3e25bcd10fbe59e51affff2a03
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-01-05 12:44:14 +0600

    password functions

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import * as Yup from 'yup';
 
 const PasswordSchema = Yup.object().shape({
@@ -10,12 +10,62 @@ const PasswordSchema = Yup.object().shape({
 });
 
 const App = () => {
+  const [password, setPassword] = useState('');
+  const [isPasswordGenerated, setIsPasswordGenerated] = useState(false);
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
+  const generatePasswordString = (passwordLength: number) => {
+    let characterList = '';
+
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const digitChars = '0123456789';
+    const specialChars = '!@#$%^&*()_+';
+
+    if (upperCase) {
+      characterList += upperCase;
+    }
+    if (lowerCase) {
+      characterList += lowerCase;
+    }
+    if (numbers) {
+      characterList += numbers;
+    }
+    if (symbols) {
+      characterList += symbols;
+    }
+
+    const passwordResult = createPassword(characterList, passwordLength);
+
+    setPassword(passwordResult);
+    setIsPasswordGenerated(true);
+  };
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
+
+  const resetPasswordState = () => {
+    setPassword('');
+    setIsPasswordGenerated(false);
+    setUpperCase(false);
+    setLowerCase(true);
+    setNumbers(false);
+    setSymbols(false);
+  };
+
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <Text>App</Text>
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+      <Text>App</Text>
+    </View>
   );
 };
 
