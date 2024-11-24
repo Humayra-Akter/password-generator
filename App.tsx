8da03035e422a6c753ed65ca6fc9ac2dc6a0534c
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-11-24 20:09:34 +0600

    readme

--- App.tsx
+++ App.tsx
@@ -155,7 +155,7 @@ export default function App() {
                   <TouchableOpacity
                     disabled={!isValid}
                     style={styles.primaryBtn}
-                    onPress={handleSubmit}>
+                    onPress={() => handleSubmit()}>
                     <Text style={styles.primaryBtnTxt}>Generate Password</Text>
                   </TouchableOpacity>
                   <TouchableOpacity
